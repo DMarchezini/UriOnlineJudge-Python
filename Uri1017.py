5a0c70afe7b56c934c5db96487f84ce02d2f2e2c
DMarchezini <79646282+DMarchezini@users.noreply.github.com>
2021-03-25 09:32:22 -0300

    Create Uri1017.py

---
+++ Uri1017.py
@@ -0,0 +1,13 @@
+"""
+Exemplo de Entrada	Exemplo de Saída
+10                      70.833
+85
+
+2                       15.333
+92
+
+22                      122.833
+67
+"""
+res = format((int(input()) * int(input())) / 12, '.3f')
+print(res)
